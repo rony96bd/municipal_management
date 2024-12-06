cb336a6591e67fd66676462c1e1f137b521baaa5
Md. Rakib Uddin <r.rony9966@gmail.com>
2024-12-06 20:23:16 +0600

    css include

--- resources/views/css-js-loaders/load-css.blade.php
+++ resources/views/css-js-loaders/load-css.blade.php
@@ -1,5 +1,7 @@
 <link rel="stylesheet" href="{{asset('css/main.min.css')}}">
 <link rel="stylesheet" href="{{asset('css/site-css.min.css')}}">
+<link rel="stylesheet" href="../repositories/municipal_management/public/css/main.min.css">
+
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Anek+Bangla:wght@100..800&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Parkinsans:wght@300..800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Tiro+Bangla:ital@0;1&display=swap');
     </style>
