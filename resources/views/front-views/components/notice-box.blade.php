a82d61233950836fe23aea65356cb3a80baab09e
Forayeji Creative Agency <112417112+forayejicreativeagency@users.noreply.github.com>
2024-12-21 19:55:37 +0600

    Home page notice box added

---
+++ resources/views/front-views/components/notice-box.blade.php
@@ -0,0 +1,30 @@
+<div class="noticebox border-gray background-gray padar-30 bradius-6px gap-60 flex column">
+    <h3 class="sidebar-title font-weight-medium padl-100">নোটিশ বোর্ড</h3>
+    <div class="flex column gap-10">
+        {{-- Single Notice Box --}}
+        <a href="#" class="anchor notice-anchor flex column gap-0 padar-10 bradius-3px">
+            <h3 class="notice-title color-primary">০৯নং ওয়ার্ড কমিটি পুনঃ গঠন।</h3>
+            <p class="notice-date color-paragraph fs-base">বৃহস্পতিবার, সেপ্টেম্বর ৭, ২০২৩ : ৫:৪৩ পূর্বাহ্ণ </p>
+        </a>
+        {{-- Single Notice Box --}}
+        <a href="#" class="anchor notice-anchor flex column gap-0 padar-10 bradius-3px">
+            <h3 class="notice-title color-primary">০৯নং ওয়ার্ড কমিটি পুনঃ গঠন।</h3>
+            <p class="notice-date color-paragraph fs-base">বৃহস্পতিবার, সেপ্টেম্বর ৭, ২০২৩ : ৫:৪৩ পূর্বাহ্ণ </p>
+        </a>
+        {{-- Single Notice Box --}}
+        <a href="#" class="anchor notice-anchor flex column gap-0 padar-10 bradius-3px">
+            <h3 class="notice-title color-primary">০৯নং ওয়ার্ড কমিটি পুনঃ গঠন।</h3>
+            <p class="notice-date color-paragraph fs-base">বৃহস্পতিবার, সেপ্টেম্বর ৭, ২০২৩ : ৫:৪৩ পূর্বাহ্ণ </p>
+        </a>
+        {{-- Single Notice Box --}}
+        <a href="#" class="anchor notice-anchor flex column gap-0 padar-10 bradius-3px">
+            <h3 class="notice-title color-primary">০৯নং ওয়ার্ড কমিটি পুনঃ গঠন।</h3>
+            <p class="notice-date color-paragraph fs-base">বৃহস্পতিবার, সেপ্টেম্বর ৭, ২০২৩ : ৫:৪৩ পূর্বাহ্ণ </p>
+        </a>
+        {{-- Single Notice Box --}}
+        <a href="#" class="anchor notice-anchor flex column gap-0 padar-10 bradius-3px">
+            <h3 class="notice-title color-primary">০৯নং ওয়ার্ড কমিটি পুনঃ গঠন।</h3>
+            <p class="notice-date color-paragraph fs-base">বৃহস্পতিবার, সেপ্টেম্বর ৭, ২০২৩ : ৫:৪৩ পূর্বাহ্ণ </p>
+        </a>
+    </div>
+</div>
---
+++ resources/views/front-views/components/sidebar.blade.php
@@ -0,0 +1,101 @@
+{{-- sidebar --}}
+<div class="sidebar-box flex column background-white padar-20 gap-40 bradius-6px background-gray border-gray">
+    {{-- Sidebar Image If needed --}}
+    <img src="{{ asset('/images/assets/logo.png') }}" alt="Image Name" class="image sidebar-image">
+    {{-- Single Sidebar Menu Repeat --}}
+    <div class="sidebar-menu flex column gap-20">
+        <div class="single-sidebar flex column gap-20">
+            <div class="sidebar-title-area flex row gap-10 jst-ace full-width padar-10 position-relative">
+                @include('icons.frontend-icons.recent-posts')
+                <h3 class="sidebar-title font-weight-medium">প্রয়োজনীয় তথ্য</h3>
+            </div>
+            <ul class="sidebar-list flex column gap-5 full-width">
+                <li class="sidebar-list-item">
+                    <a href="#"
+                        class="sidebar-list-link color-primary flex padl-30 padr-20 padt-10 padb-10 position-relative fs-base">হোল্ডিং
+                        ট্যাক্স ইনফরমেশন</a>
+                </li>
+                <li class="sidebar-list-item">
+                    <a href="#"
+                        class="sidebar-list-link color-primary flex padl-30 padr-20 padt-10 padb-10 position-relative fs-base">ওয়াটার
+                        বিল ইনফরমেশন</a>
+                </li>
+                <li class="sidebar-list-item">
+                    <a href="#"
+                        class="sidebar-list-link color-primary flex padl-30 padr-20 padt-10 padb-10 position-relative fs-base">ট্রেড
+                        লাইসেন্স ইনফরমেশন</a>
+                </li>
+                <li class="sidebar-list-item">
+                    <a href="#"
+                        class="sidebar-list-link color-primary flex padl-30 padr-20 padt-10 padb-10 position-relative fs-base">স্টাফ
+                        ইনফরমেশন</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+    {{-- Single Sidebar Menu Repeat --}}
+    <div class="sidebar-menu flex column gap-20">
+        <div class="single-sidebar flex column gap-20">
+            <div class="sidebar-title-area flex row gap-10 jst-ace full-width padar-10 position-relative">
+                @include('icons.frontend-icons.recent-posts')
+                <h3 class="sidebar-title font-weight-medium">কেন্দ্রীয় ই-সেবা</h3>
+            </div>
+            <ul class="sidebar-list flex column gap-5 full-width">
+                <li class="sidebar-list-item">
+                    <a href="#"
+                        class="sidebar-list-link color-primary flex padl-30 padr-20 padt-10 padb-10 position-relative fs-base">হোল্ডিং
+                        প্রধানমন্ত্রীর কার্যালয়</a>
+                </li>
+                <li class="sidebar-list-item">
+                    <a href="#"
+                        class="sidebar-list-link color-primary flex padl-30 padr-20 padt-10 padb-10 position-relative fs-base">ওয়াটার
+                        জন প্রশাসন মন্ত্রণালয়</a>
+                </li>
+                <li class="sidebar-list-item">
+                    <a href="#"
+                        class="sidebar-list-link color-primary flex padl-30 padr-20 padt-10 padb-10 position-relative fs-base">ট্রেড
+                        লাইসেন্স ইনফরমেশন</a>
+                </li>
+                <li class="sidebar-list-item">
+                    <a href="#"
+                        class="sidebar-list-link color-primary flex padl-30 padr-20 padt-10 padb-10 position-relative fs-base">স্টাফ
+                        ইনফরমেশন</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+    {{-- Single Sidebar Menu Repeat --}}
+    <div class="sidebar-menu flex column gap-20">
+        <div class="single-sidebar flex column gap-20">
+            <div class="sidebar-title-area flex row gap-10 jst-ace full-width padar-10 position-relative">
+                @include('icons.frontend-icons.recent-posts')
+                <h3 class="sidebar-title font-weight-medium">কারিগরি সহায়তা</h3>
+            </div>
+            <ul class="sidebar-list flex column gap-5 full-width">
+                <li class="sidebar-list-item">
+                    <a href="#"
+                        class="sidebar-list-link color-primary flex padl-30 padr-20 padt-10 padb-10 position-relative fs-base">হোল্ডিং
+                        প্রধানমন্ত্রীর কার্যালয়</a>
+                </li>
+                <li class="sidebar-list-item">
+                    <a href="#"
+                        class="sidebar-list-link color-primary flex padl-30 padr-20 padt-10 padb-10 position-relative fs-base">ওয়াটার
+                        জন প্রশাসন মন্ত্রণালয়</a>
+                </li>
+                <li class="sidebar-list-item">
+                    <a href="#"
+                        class="sidebar-list-link color-primary flex padl-30 padr-20 padt-10 padb-10 position-relative fs-base">ট্রেড
+                        লাইসেন্স ইনফরমেশন</a>
+                </li>
+                <li class="sidebar-list-item">
+                    <a href="#"
+                        class="sidebar-list-link color-primary flex padl-30 padr-20 padt-10 padb-10 position-relative fs-base">স্টাফ
+                        ইনফরমেশন</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</div>
