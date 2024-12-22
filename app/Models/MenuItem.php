e407de55d20552ba5a703f26d1ab07b5e03087bc
Md. Rakib Uddin <r.rony9966@gmail.com>
2024-12-22 13:44:40 +0600

    Menu Builder_1

---
+++ app/Models/MenuItem.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MenuItem extends Model
+{
+    use HasFactory;
+
+    public function menu()
+    {
+        return $this->belongsTo(Menu::class);
+    }
+}
