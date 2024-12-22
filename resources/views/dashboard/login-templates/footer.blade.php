019196634962ebb2932a291926d0a2eecde5890c
Forayeji Creative Agency <112417112+forayejicreativeagency@users.noreply.github.com>
2024-12-22 15:59:06 +0600

    Small Change on template

--- resources/views/dashboard/login-templates/footer.blade.php
+++ resources/views/dashboard/login-templates/footer.blade.php
@@ -1,4 +1,9 @@
-</main>
-</body>
+<p class="color-primary text-center">Developed & Maintained by: <a href="https://forayeji.com" target="_blank"
+        class="color-secondary">Forayeji
+        Creative
+        Agency</a></p>
+<p>
+    </main>
+    </body>
 
-</html>
+    </html>
--- resources/views/dashboard/login.blade.php
+++ resources/views/dashboard/login.blade.php
@@ -45,9 +45,4 @@
             </div>
         </form>
     </div>
-    <p class="color-primary text-center">Developed & Maintained by: <a href="https://forayeji.com" target="_blank"
-            class="color-secondary">Forayeji
-            Creative
-            Agency</a></p>
-    <p>
-    @endsection
+@endsection
