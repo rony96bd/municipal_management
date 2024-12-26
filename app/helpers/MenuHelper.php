5d867d2765840cc861d52f241d878d0b8089cecc
Md. Rakib Uddin <r.rony9966@gmail.com>
2024-12-26 19:20:10 +0600

    Update MenuHelper.php

--- app/helpers/MenuHelper.php
+++ app/helpers/MenuHelper.php
@@ -14,10 +14,6 @@ if (!function_exists('menu')) {
     function menu($name)
     {
         $menu = \App\Models\Menu::where('name', $name)->first();
-        if ($menu == null) {
-            echo "কোন মেনু পাওয়া যায়নি";
-        } else {
-            return $menu->menuItems()->with('childs')->get();
-        }
+        return $menu->menuItems()->with('childs')->get();
     }
 }
