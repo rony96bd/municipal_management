9ffc0acb1617c3b0fd76b4241609a14fc3e9f336
Md. Rakib Uddin <r.rony9966@gmail.com>
2024-12-26 14:42:06 +0600

    test for server

--- app/helpers/MenuHelper.php
+++ app/helpers/MenuHelper.php
@@ -14,6 +14,10 @@ if (!function_exists('menu')) {
     function menu($name)
     {
         $menu = \App\Models\Menu::where('name', $name)->first();
-        return $menu->menuItems()->with('childs')->get();
+        if ($menu == null) {
+            echo "কোন মেনু পাওয়া যায়নি";
+        } else {
+            return $menu->menuItems()->with('childs')->get();
+        }
     }
 }
--- resources/views/components/frontend-menu-bar.blade.php
+++ resources/views/components/frontend-menu-bar.blade.php
@@ -1,39 +1,39 @@
-{{-- @if ($items == null)
-@else --}}
-    @foreach ($items as $item)
-        @if ($item->type == 'divider')
-            <li class="">{{ $item->divider_title }}</li>
+@php
+dd($items);
+@endphp
+@foreach ($items as $item)
+    @if ($item->type == 'divider')
+        <li class="">{{ $item->divider_title }}</li>
+    @else
+        @if ($item->childs->isEmpty())
+            <li
+                class="llisting includs-submenu flex row center width-max-content position-relative transition-ease transition">
+                <a href="{{ $item->url }}"
+                    class="{{ Request::is(ltrim($item->url, '/') . '*') ? 'mm-active' : '' }} anchor color-link z-index-2 position-relative">
+                    {{ $item->title }}
+                </a>
+            </li>
         @else
-            @if ($item->childs->isEmpty())
-                <li
-                    class="llisting includs-submenu flex row center width-max-content position-relative transition-ease transition">
-                    <a href="{{ $item->url }}"
-                        class="{{ Request::is(ltrim($item->url, '/') . '*') ? 'mm-active' : '' }} anchor color-link z-index-2 position-relative">
-                        {{ $item->title }}
-                    </a>
-                </li>
-            @else
-                <li class="llisting includs-submenu flex row center width-max-content position-relative transition-ease transition"
-                    @foreach ($item->childs as $child) @if (Request::is(ltrim($child->url, '/') . '*')) class="mm-active" @break @endif @endforeach>
-                    <a href="{{ $item->url }}"
-                        class="{{ Request::is(ltrim($item->url, '/') . '*') ? 'mm-active' : '' }} anchor color-link z-index-2 position-relative">
-                        {{ $item->title }}
-                    </a>
-                    <img src="{{ asset('images/assets/arrow-down.svg') }}" alt="arrow down"
-                        class="nav-icon z-index-2 position-relative">
-                    <ul
-                        class="sub-menu position-absolute left-0 top-0 padt-50 transition-ease-in-out transition-duration-0-25s transition-property-all z-index-1 padl-20 padr-20 padb-10 gap-10 flex column">
-                        @foreach ($item->childs as $child)
-                            <li class="anchor color-link">
-                                <a href="{{ $child->url }}"
-                                    class="{{ Request::is(ltrim($child->url, '/') . '*') ? 'mm-active' : '' }}">
-                                    {{ $child->title }}
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </li>
-            @endif
+            <li class="llisting includs-submenu flex row center width-max-content position-relative transition-ease transition"
+                @foreach ($item->childs as $child) @if (Request::is(ltrim($child->url, '/') . '*')) class="mm-active" @break @endif @endforeach>
+                <a href="{{ $item->url }}"
+                    class="{{ Request::is(ltrim($item->url, '/') . '*') ? 'mm-active' : '' }} anchor color-link z-index-2 position-relative">
+                    {{ $item->title }}
+                </a>
+                <img src="{{ asset('images/assets/arrow-down.svg') }}" alt="arrow down"
+                    class="nav-icon z-index-2 position-relative">
+                <ul
+                    class="sub-menu position-absolute left-0 top-0 padt-50 transition-ease-in-out transition-duration-0-25s transition-property-all z-index-1 padl-20 padr-20 padb-10 gap-10 flex column">
+                    @foreach ($item->childs as $child)
+                        <li class="anchor color-link">
+                            <a href="{{ $child->url }}"
+                                class="{{ Request::is(ltrim($child->url, '/') . '*') ? 'mm-active' : '' }}">
+                                {{ $child->title }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </li>
         @endif
-    @endforeach
-{{-- @endif --}}
+    @endif
+@endforeach
