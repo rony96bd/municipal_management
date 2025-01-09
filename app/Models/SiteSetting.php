99b90790170dd16ffdb3d13a027b94a85f6cfa84
Forayeji Creative Agency <112417112+forayejicreativeagency@users.noreply.github.com>
2025-01-09 11:36:54 +0600

    File Renamed

--- app/Models/SiteSetting.php
+++ app/Models/SiteSetting.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SiteSettings extends Model
+class SiteSetting extends Model
 {
     use HasFactory;
 
