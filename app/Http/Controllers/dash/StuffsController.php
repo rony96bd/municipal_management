1bab125bb6df1a38cb0bf710e50e796dc627be0f
Md. Rakib Uddin <r.rony9966@gmail.com>
2025-02-19 19:04:28 +0600

    fix: add section handling in store and update methods of StuffsController

--- app/Http/Controllers/dash/StuffsController.php
+++ app/Http/Controllers/dash/StuffsController.php
@@ -72,6 +72,7 @@ class StuffsController extends Controller
         $stuff->first_designation = $request->first_designation;
         $stuff->joining_date = $request->joining_date;
         $stuff->grade = $request->grade;
+        $stuff->section = $request->section;
         $stuff->page_url = $slug . '-' . $randomNumber; // Assign the unique page_url
         $stuff->image = $imagePath;
         $stuff->save();
@@ -133,6 +134,7 @@ class StuffsController extends Controller
         $stuff->first_designation = $request->first_designation;
         $stuff->joining_date = $request->joining_date;
         $stuff->grade = $request->grade;
+        $stuff->section = $request->section;
         $stuff->page_url = $slug . '-' . $randomNumber;  // Assign the updated page_url
         $stuff->save();
 
