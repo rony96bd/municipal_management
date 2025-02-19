12ef2135e1e491dcb5ce38bc0d9ceb0d57ac633f
Md. Rakib Uddin <r.rony9966@gmail.com>
2025-02-19 14:43:33 +0600

    fix: adjust padding in notice box and comment out date display for improved layout

--- resources/views/front-views/components/notice-box.blade.php
+++ resources/views/front-views/components/notice-box.blade.php
@@ -2,11 +2,11 @@
     style="
     background-image: url({{ asset('/images/assets/notice-background.png') }});">
     <h3 class="sidebar-title font-weight-medium padl-100">নোটিশ বোর্ড</h3>
-    <div class="flex column gap-10">
+    <div class="flex column">
         @forelse ($notices as $notice)
             {{-- Single Notice Box --}}
             <a href="{{ url('/notice' . '/' . $notice->page_url) }}"
-                class="anchor notice-anchor flex column gap-0 padar-10 bradius-3px">
+                class="anchor notice-anchor flex column gap-0 padar-5 bradius-3px">
                 <img style="width: 20px; position: absolute;" src="{{ asset('images/assets/next.png') }}" alt="Notice Icon" class="img notice">
                 <h3 style="margin: 1px 0px 0px 25px;" class="notice-title color-primary">{{ $notice->topic }}</h3>
                 {{-- <p style="margin: 0 0 0 25px;" class="notice-date color-paragraph fs-base">বৃহস্পতিবার, সেপ্টেম্বর ৭, ২০২৩ : ৫:৪৩ পূর্বাহ্ণ </p> --}}
