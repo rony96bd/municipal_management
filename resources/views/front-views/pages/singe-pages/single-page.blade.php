ee7666ada61edb75714963e3391ce2f66ac27910
Md. Rakib Uddin <r.rony9966@gmail.com>
2025-02-19 15:38:59 +0600

    fix: add section title for single page if page name is available

--- resources/views/front-views/pages/singe-pages/single-page.blade.php
+++ resources/views/front-views/pages/singe-pages/single-page.blade.php
@@ -1,6 +1,15 @@
 @extends('front-views.templates.othe-page-header')
 @section('page-body')
     {{-- section --}}
+    @if (!empty($page->page_name))
+        <section class="section">
+            <div class="container">
+                <h2 class="section-title">{{ $page_name }}</h2>
+            </div>
+        </section>
+    @endif
+    <br/>
+    {{-- section --}}
     @if (!empty($page->page_data))
         <section class="section">
             <div class="container">
