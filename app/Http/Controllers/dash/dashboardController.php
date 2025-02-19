aff5780cad07bcf33eb4eb42885d7efbb8b83fdf
Md. Rakib Uddin <r.rony9966@gmail.com>
2025-02-19 12:49:52 +0600

    fix: retrieve site settings in dashboard controller for improved functionality

--- app/Http/Controllers/dash/dashboardController.php
+++ app/Http/Controllers/dash/dashboardController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\dash;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\SiteSettings;
 
 class dashboardController extends Controller
 {
     public function dashboard()
     {
         $page_title = 'ড্যাশবোর্ড';
+        $settings = SiteSettings::first();
         return view('dashboard.dashboard', compact('page_title'));
     }
 }
