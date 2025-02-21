06456f33ead20e9160e2bf9ed7c089200e5ddb08
Md. Rakib Uddin <r.rony9966@gmail.com>
2025-02-21 17:03:36 +0600

    feat: add search functionality and table structure for notices display

--- resources/views/front-views/pages/notices.blade.php
+++ resources/views/front-views/pages/notices.blade.php
@@ -2,7 +2,34 @@
 @section('page-body')
     <section class="section">
         <div class="container grid grid-col-5 m-grid-col-1 gap-20">
-            @forelse ($notices as $notice)
+            <div class="container mt-5">
+                <h1>Notice</h1>
+                <form action="" method="GET" class="mb-4">
+                    <div class="input-group">
+                        <input type="text" name="search" class="form-control" placeholder="Search products..."
+                            value="">
+                        <button type="submit" class="btn btn-primary">Search</button>
+                    </div>
+                </form>
+
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            {{-- @forelse ($notices as $notice)
                 <a href="{{ url('/') }}/notice/{{ $notice->page_url }}" class="flex column gap-0 overflow-hidden bradius-10px border-solid border-1px border-gray">
                     <div class="flex column padar-20 background-gray flex-auto full-width jfs-ais gap-0">
                         @if (!empty($notice->topic))
@@ -20,7 +47,7 @@
 
                 </a>
             @empty
-            @endforelse
+            @endforelse --}}
         </div>
     </section>
 @endsection
