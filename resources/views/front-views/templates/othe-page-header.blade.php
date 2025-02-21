456ae0c78455aee30a2ec87109e0b9dd841961f9
Md. Rakib Uddin <r.rony9966@gmail.com>
2025-02-21 17:05:09 +0600

    feat: add Bootstrap CSS for improved styling in page header

--- resources/views/front-views/templates/othe-page-header.blade.php
+++ resources/views/front-views/templates/othe-page-header.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="{{ asset('developers.png') }}" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     @include('css-js-loaders.load-css')
 
     <title>
