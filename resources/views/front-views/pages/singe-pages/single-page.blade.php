b8a90059b1c1dde46baf38d28be0f494ed4c1594
Md. Rakib Uddin <r.rony9966@gmail.com>
2025-02-24 12:46:28 +0600

    feat: restructure single page view for improved layout and readability

--- resources/views/front-views/pages/singe-pages/single-page.blade.php
+++ resources/views/front-views/pages/singe-pages/single-page.blade.php
@@ -1,13 +1,17 @@
 @extends('front-views.templates.othe-page-header')
 @section('page-body')
     {{-- section --}}
-    @if (!empty($page->page_name))
-        <section class="section">
-            <p><h2 class="section-title">{{ $page_title }}</h2></p>
-    @endif
-    @if (!empty($page->page_data))
-            <p>{!! $page->page_data !!}</p>
-            <p><iframe src="{{ url($page->file_path) }}" width="100%" height="600px"></iframe></p>
-    @endif
-</section>
+
+    <section class="section">
+        <div class="container">
+            <div class="flex column gap-20">
+                <h2 class="text-center">{{ $page->page_name }}</h2>
+                <div class="flex column gap-20">
+                    <div class="flex column gap-20">
+                        <p>{!! $page->page_data !!}</p>
+                        <p><iframe src="{{ url($page->file_path) }}" width="100%" height="600px"></iframe></p>
+                    </div>
+                </div>
+            </div>
+    </section>
 @endsection
