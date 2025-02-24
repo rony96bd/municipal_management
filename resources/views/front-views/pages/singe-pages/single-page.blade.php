923353130d850d5f5df72c8f5293c9d7e8217fe4
Md. Rakib Uddin <r.rony9966@gmail.com>
2025-02-24 12:43:07 +0600

    feat: refactor single page view to improve section structure and display content in paragraphs

--- resources/views/front-views/pages/singe-pages/single-page.blade.php
+++ resources/views/front-views/pages/singe-pages/single-page.blade.php
@@ -3,15 +3,11 @@
     {{-- section --}}
     @if (!empty($page->page_name))
         <section class="section">
-            <div class="container">
-                <h2 class="section-title">{{ $page_title }}</h2>
-            </div>
+            <p><h2 class="section-title">{{ $page_title }}</h2></p>
     @endif
     @if (!empty($page->page_data))
-            <div class="container">
-                {!! $page->page_data !!}
-            </div>
-            <div><iframe src="{{ url($page->file_path) }}" width="100%" height="600px"></iframe></div>
+            <p>{!! $page->page_data !!}</p>
+            <p><iframe src="{{ url($page->file_path) }}" width="100%" height="600px"></iframe></p>
     @endif
 </section>
 @endsection
