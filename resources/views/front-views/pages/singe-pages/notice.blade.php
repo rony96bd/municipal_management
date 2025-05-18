d92653af1ab1a86476d84d5d2e48ecc38edcfbf2
Md. Rakib Uddin <r.rony9966@gmail.com>
2025-05-18 18:03:07 +0600

    Remove magnifier effect and related styles from notice.blade.php and footer.blade.php

--- resources/views/front-views/pages/singe-pages/notice.blade.php
+++ resources/views/front-views/pages/singe-pages/notice.blade.php
@@ -14,32 +14,10 @@
                         @endphp
 
                         @if (in_array(strtolower($extension), ['jpg', 'jpeg', 'png', 'gif', 'webp']))
-                            <style>
-                                .img-magnifier-container {
-                                    position: relative;
-                                }
-
-                                .img-magnifier-glass {
-                                    position: absolute;
-                                    border: 3px solid #000;
-                                    border-radius: 50%;
-                                    cursor: none;
-                                    width: 100px;
-                                    height: 100px;
-                                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-                                    background-repeat: no-repeat;
-                                    background-position: center;
-                                    background-size: 200%;
-                                    z-index: 1000;
-                                    display: none;
-                                }
-                            </style>
-
-                            <div class="img-magnifier-container">
-                                <img id="image" src="{{ url($notice->file_path) }}" alt="Uploaded Image"
+                            <p>
+                                <img src="{{ url($notice->file_path) }}" alt="Uploaded Image"
                                     style="max-width: 100%; height: auto;">
-                                <div id="magnifier" class="img-magnifier-glass"></div>
-                            </div>
+                            </p>
                         @elseif(strtolower($extension) === 'pdf')
                             <p>
                                 <iframe src="{{ url($notice->file_path) }}" width="100%" height="600px"></iframe>
@@ -47,7 +25,6 @@
                         @else
                             <p>Unsupported file type.</p>
                         @endif
-
                     </div>
                 </div>
             </div>
