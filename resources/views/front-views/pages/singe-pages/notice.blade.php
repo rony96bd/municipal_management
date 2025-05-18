75c77acd6795183c5eb8e596e803ad3064e893bb
Md. Rakib Uddin <r.rony9966@gmail.com>
2025-05-18 17:49:58 +0600

    Refactor file type handling in notice.blade.php to display images and PDFs correctly

--- resources/views/front-views/pages/singe-pages/notice.blade.php
+++ resources/views/front-views/pages/singe-pages/notice.blade.php
@@ -9,7 +9,23 @@
                     <div class="flex column gap-20">
                         <h3>বিষয়: {{ $notice->topic }}</h3>
                         <p>{!! $notice->description !!}</p>
-                        <p><iframe src="{{ url($notice->file_path) }}" width="100%" height="600px"></iframe></p>
+                        @php
+                            $extension = pathinfo($notice->file_path, PATHINFO_EXTENSION);
+                        @endphp
+
+                        @if (in_array(strtolower($extension), ['jpg', 'jpeg', 'png', 'gif', 'webp']))
+                            <p>
+                                <img src="{{ url($notice->file_path) }}" alt="Uploaded Image"
+                                    style="max-width: 100%; height: auto;">
+                            </p>
+                        @elseif(strtolower($extension) === 'pdf')
+                            <p>
+                                <iframe src="{{ url($notice->file_path) }}" width="100%" height="600px"></iframe>
+                            </p>
+                        @else
+                            <p>Unsupported file type.</p>
+                        @endif
+
                     </div>
                 </div>
             </div>
