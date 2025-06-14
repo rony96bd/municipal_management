cd91c4b105cc9e9e289aafc3eb310a14bb1d85e7
Md. Rakib Uddin <r.rony9966@gmail.com>
2025-06-14 15:45:43 +0600

    Refactor news display in news.blade.php to use dynamic colors and improve loop structure

--- resources/views/front-views/components/news.blade.php
+++ resources/views/front-views/components/news.blade.php
@@ -1,5 +1,4 @@
 @if ($news->isNotEmpty())
-
     <style>
         .news-item:nth-child(1) {
             background-color: #1A237E;
@@ -39,9 +38,11 @@
 
         /* Repeat for more items or randomize */
     </style>
+
     @php
         $colors = ['#1A237E', '#D32F2F', '#0288D1', '#388E3C', '#F57C00', '#8E24AA']; // Deep colors
     @endphp
+
     <div class="flex column overflow-hidden bradius-6px background-gray">
         <div
             class="section-title flex row gap-10 jsb-ace padl-30 padr-30 padt-10 padb-10 background-secondary color-white m-padl-10 m-padr-10 m-padr-10">
@@ -53,13 +54,24 @@
             {{-- Single Service --}}
             <div class="slidder-wrapper-container padt-10 padb-10">
                 <div class="slidder-wrapper flex row gap-5 no-wrap">
-                    @foreach ($news as $index => $singnews)
+                    @forelse ($news as $singnews)
                         <a href="{{ url('/news') . '/' . $singnews->page_url }}"
-                            class="single-slidder-item anchor news-item flex row gap-5 center"
-                            style="background-color: {{ $colors[$index % count($colors)] }};">
+                            class="single-slidder-item anchor news-item flex row gap-5 center" style="color: {{ $colors[$index % count($colors)] }};">
                             {{ $singnews->topic }}
                         </a>
-                    @endforeach
+                    @empty
+                        <p>কোন নিউজ যুক্ত করা নেয়</p>
+                    @endforelse
+
+                    {{-- Duplicate news items for seamless loop --}}
+                    @forelse ($news as $singnews)
+                        <a href="{{ url('/news') . '/' . $singnews->page_url }}"
+                            class="single-slidder-item anchor news-item flex row gap-5 center">
+                            {{ $singnews->topic }}
+                        </a>
+                    @empty
+                        <p>কোন নিউজ যুক্ত করা নেয়</p>
+                    @endforelse
                 </div>
             </div>
         </div>
